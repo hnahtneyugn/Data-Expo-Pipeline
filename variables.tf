9bed7f9c37337c7b89bd6ea2d89c3158c27296e1
hnahtneyugn <tanguyenthanh1969@gmail.com>
2025-08-05 18:22:48 +0700

    completed testing ingestion script

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  description = "My Credentials"
+  type        = string
+}
+
+variable "project" {
+  description = "My Main Project"
+  type        = string
+}
+
+variable "region" {
+  description = "Project Region"
+  type        = string
+}
+
+variable "location" {
+  description = "Project Location"
+  type        = string
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  type        = string
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  type        = string
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  type        = string
+}
